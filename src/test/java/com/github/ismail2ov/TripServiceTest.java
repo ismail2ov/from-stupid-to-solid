c679b7719f799e380378a27d62f969bf78ec8237
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-05-12 01:14:35 +0200

    Added the concept of Guest user

--- src/test/java/com/github/ismail2ov/TripServiceTest.java
+++ src/test/java/com/github/ismail2ov/TripServiceTest.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TripServiceTest {
+    public static final User GUEST = null;
+    protected User loggedUser = null;
+
     @Test
     void should_throw_exception_when_user_is_not_logged_in() {
         TripService tripService = new TestableTripService();
-        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+        loggedUser = GUEST;
+
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(GUEST));
     }
 
     private class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedUser;
         }
     }
 }
